e810b8535c5b68cfb7cb0e84da597867857d5ef8
nankuF <fireloki87@gmail.com>
2022-03-12 20:16:48 +0300

    fix payload

--- api_weather.py
+++ api_weather.py
@@ -3,7 +3,7 @@ import requests
 
 def get_weather(place: str):
     url = f'http://wttr.in/{place}'
-    payload = {'nTqu': '', 'lang': 'ru', 'm': '', }
+    payload = {'nTqm': '', 'lang': 'ru'}
     response = requests.get(url, params=payload)
     response.raise_for_status()
     return response.text
